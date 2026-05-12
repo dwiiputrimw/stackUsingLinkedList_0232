ad92e9e9929438c774fb3e0297428cf762b5e362
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-05-12 10:37:25 +0700

    Menambahkan output push operation

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -30,4 +30,8 @@ public:
         Node *newNode = new Node();
         newNode->data = value;
         newNode->next = top;
+        cout << "Push value: " << value << endl;
+        return value; 
+    }
+
 };
